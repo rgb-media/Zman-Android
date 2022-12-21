e404368a96faaabc99996e86c99d493f29fa493c
AdrianPicui <adrian.picui@gmail.com>
2022-12-21 15:50:30 +0200

    Add Zman Twitter account and change to WWW

--- app/src/main/java/com/rgbmedia/zman/Constants.kt
+++ app/src/main/java/com/rgbmedia/zman/Constants.kt
@@ -1,11 +1,11 @@
 package com.rgbmedia.zman
 
 const val DOMAIN_NAME   = "zman.co.il"
-const val HOMEPAGE_URL  = "https://dev.zman.co.il/"
+const val HOMEPAGE_URL  = "https://www.$DOMAIN_NAME/"
 const val MENU_URL      = "https://dl.dropbox.com/s/srfbc9amivphi2q/"
-const val SEARCH_URL    = "https://www.zman.co.il/wp-content/themes/rgb/ajax/"
-const val LOGIN_URL     = "https://dev.zman.co.il/wp-content/themes/rgb/ajax/"
-const val SENDGRID_URL  = "https://dev.zman.co.il/wp-content/themes/rgb/functions/sendgrid/"
+const val SEARCH_URL    = HOMEPAGE_URL + "wp-content/themes/rgb/ajax/"
+const val LOGIN_URL     = HOMEPAGE_URL + "wp-content/themes/rgb/ajax/"
+const val SENDGRID_URL  = HOMEPAGE_URL + "wp-content/themes/rgb/functions/sendgrid/"
 
 const val MENU_ANIMATION_DURATION = 700L
 
